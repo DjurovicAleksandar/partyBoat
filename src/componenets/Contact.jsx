41046bb2142cc73095cf021d4196c84bad9b56b3
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-06-20 13:45:10 +0200

    Fix of letter

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -1,28 +1,22 @@
 import contactBg from "../assets/img/contact/contactBg.png";
-import blackPhone from "../assets/img/contact/blackPhone.png";
 import ContactOptions from "./contactComponents/ContactOptions";
-import { useEffect, useState } from "react";
 
 function Contact() {
-  console.log(window.innerWidth < 600);
   return (
     <div id="contact" className="my-10">
       <section className="py-6 text-gray-50">
         <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
           <div className="w-full relative">
             <div className="aspect-square flex flex-col items-center justify-center mr-4 p-4 absolute inset-0 w-full h-full z-50 font-bold">
-              <h1 className="text-xl md:text-4xl font-bold text-[#f24603e0] bg-black/70 py-2 px-4 rounded-md">
+              <h2 className="text-xl md:text-4xl font-bold text-[#f24603e0] bg-black/70 py-2 px-4 rounded-md">
                 Get in touch
-              </h1>
-              <p className="pt-2 pb-4 text-[#f24603e0]  bg-black/70 py-1 px-2 rounded-md">
-                Fill in the form to start a conversation
-              </p>
+              </h2>
               <ContactOptions />
             </div>
             <div className="relative">
               <div className="absolute inset-0 backdrop-blur-sm hidden lg:block bg-[#f24603e0]/60 w-[20rem]"></div>
               <img
-                src={window.innerWidth > 650 ? contactBg : blackPhone}
+                src={contactBg}
                 alt="phone"
                 className="lg:-translate-y-[5rem] lg:-translate-x-[18rem] z-0 translate-x-2 -translate-y-[13rem]"
               />
@@ -33,6 +27,9 @@ function Contact() {
             method="POST"
             className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid"
           >
+            <h2 className="pt-2 pb-4 text-[#f24603e0]  bg-black/70 py-1 px-2 rounded-md text-xl text-center font-bold">
+              Fill in the form to start a conversation
+            </h2>
             <label className="block">
               <span className="mb-1">Full name</span>
               <input
